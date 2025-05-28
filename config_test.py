31702089420efa25d3a3cfc680f61d01ad4dafb1
wjhj123 <77092703+wjhj123@users.noreply.github.com>
2025-05-28 14:21:13 +0800

    Add files via upload

---
+++ config_test.py
@@ -0,0 +1,6 @@
+
+
+TEST_FILENAME = r"D:\Users\Administrator\PycharmProjects\US\home\test\317_xg_test.npz"
+TEST_DIR = r"H:\dataset_code\tuer\528test_UIU_Net"
+BEST_MODEL = r"D:\Users\Administrator\PycharmProjects\US\home\PA_US\model\best_metric_oringinal_UIU_Net_model_317_xg_20000.pth"
+TRAIN_MODEL = "UIUNET"
